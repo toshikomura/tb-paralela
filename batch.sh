8ba262578cf6ff578f45558d55a8b494fef33b77
Gustavo Toshi Komura <gtk10@c3sl.ufpr.br>
2014-06-05 18:34:15 -0300

    Change "batch.sh" to execute all tests
    Signed-off-by: Gustavo Toshi Komura <gtk10@c3sl.ufpr.br>

--- batch.sh
+++ batch.sh
@@ -3,27 +3,29 @@
 # função para testar se os parametros estão corretos
 function usage {
 	echo uso:
-	echo ${0} '{tamanho da grade} "{repetições}" {threadini} {threadfim}'
+	echo ${0} '"{repetições}" {threadini} {threadfim}'
 
 	exit 1
 }
 
 # Testa se foi passado o número correto de paramentos
-if [ ! ${#} -eq 4 ] ;	then
+if [ ! ${#} -eq 3 ] ;	then
 	usage
 fi
 
-grade=$1
-iteracoes=$2
-threadini=$3
-threadfim=$4
+iteracoes=$1
+threadini=$2
+threadfim=$3
 
-for cores in 1 2 4 8 ; do
-	for num in $(seq ${iteracoes}) ; do
-		likwid-pin -q -c ${threadini}-${threadfim} ./rbgs ${grade} ${grade} ${cores} 2000 g >> pass${grade}x${cores}.txt
-	done
-	echo ${cores}
-	./media pass${grade}x${cores}.txt
-	./media pass${grade}x${cores}.txt >> testes/medias${grade}x${cores}.txt
-	rm -f pass${grade}x${cores}.txt
+for grade in 32 33 1024 1025 2048 2049 ; do
+    for cores in 8 4 2 1 ; do
+	    for num in $(seq ${iteracoes}) ; do
+		    likwid-pin -q -c ${threadini}-${threadfim} ./rgbs ${grade} ${grade} ${cores} 2000 g >> pass${grade}x${cores}.txt
+	    done
+	    echo "${grade} ${cores}"
+        echo "${grade} ${cores}" >> testes/medias${grade}x${cores}.txt
+	    ./media pass${grade}x${cores}.txt
+	    ./media pass${grade}x${cores}.txt >> testes/medias${grade}x${cores}.txt
+	    rm -f pass${grade}x${cores}.txt
+    done
 done
